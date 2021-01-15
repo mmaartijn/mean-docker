fe8ec17a379a285de9670196a92439ae4fbe933e
mmaartijn <mmaartijn@gmail.com>
2021-01-15 16:08:28 +0100

    .

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
-require("dotenv").config();
-let express = require("express");
-let app = express();
-const environment = require("./config/environment");
-let cors = require("cors");
-let path = require("path");
-let bodyParser = require("body-parser");
-let expressJwt = require("express-jwt");
+/* eslint-disable no-console */
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressJwt = require('express-jwt');
 // Import Mongoose
-let mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const environment = require('./config/environment');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,74 +18,73 @@ app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
 // MongoDB connection
-console.log("connection string", environment.mongodb.uri);
-console.log("secret", environment.secret);
+console.log('connection string', environment.mongodb.uri);
+console.log('secret', environment.secret);
 mongoose.connect(environment.mongodb.uri, {
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 mongoose.Promise = global.Promise;
 
 // On connection error
-mongoose.connection.on("error", (error) => {
-  console.log("Database error: ", error);
+mongoose.connection.on('error', (error) => {
+  console.log('Database error: ', error);
 });
 
 // On successful connection
-mongoose.connection.on("connected", () => {
-  console.log("Connected to database");
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database');
 });
 
 // addtional configuration when serving Angular SPA (static reource and Anugalr routing)
 const allowedExt = [
-  ".js",
-  ".ico",
-  ".css",
-  ".png",
-  ".jpg",
-  ".woff2",
-  ".woff",
-  ".ttf",
-  ".svg",
-  ".webmanifest"
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+  '.webmanifest',
 ];
 
-
 // Import routes
-let apiRoutes = require("./api-routes");
+const apiRoutes = require('./api-routes');
 
-
-// use JWT auth to secure the api, the token can be passed in the authorization header or querystring
+// use JWTauth to secure the api, the token can be passed in the authorization header or querystring
 app.use(
   expressJwt({
     secret: environment.secret || 'myJWTSecret',
-    algorithms: ["HS256"],
-    getToken: function (req) {
+    algorithms: ['HS256'],
+    getToken: (req) => {
       if (
         req.headers.authorization &&
-        req.headers.authorization.split(" ")[0] === "Bearer"
+        req.headers.authorization.split(' ')[0] === 'Bearer'
       ) {
-        return req.headers.authorization.split(" ")[1];
+        return req.headers.authorization.split(' ')[1];
+      // eslint-disable-next-line no-else-return
       } else if (req.query && req.query.token) {
         return req.query.token;
       }
       return null;
-    }
-  }).unless({ path: ["/api/user/authenticate", "/api/users", "/index.html"] })
+    },
+  }).unless({ path: ['/api/user/authenticate', '/api/users', '/index.html'] }),
 );
 
 // Use Api routes in the App
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
 
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   if (allowedExt.filter((ext) => req.url.indexOf(ext) > 0).length > 0) {
     res.sendFile(path.resolve(`public/${req.url}`));
   } else {
-    res.sendFile(path.resolve("public/index.html"));
+    res.sendFile(path.resolve('public/index.html'));
   }
 });
 
-const HOST = "0.0.0.0";
+const HOST = '0.0.0.0';
 // start server
 // Launch app to listen to specified port
 const server = app.listen(process.env.EXPRESS_PORT || 3000, HOST, () => {
